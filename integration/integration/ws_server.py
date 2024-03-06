a64536e7f259921657bc2ab5882eeed0db8bd9d8
wkdev <chanhyukman@gmail.com>
2024-03-06 12:50:58 +0900

    adding ws integration

--- integration/integration/ws_server.py
+++ integration/integration/ws_server.py
@@ -6,7 +6,8 @@ from collections import deque
 import asyncio
 import websockets
 import json
-
+import threading
+import time
 class MyNode(Node):
     def __init__(self):
         super().__init__('ws_server')
@@ -16,8 +17,42 @@ class MyNode(Node):
         self.create_subscription(AGVNavStat, '/nav_stat', self.nav_stat_callback, 10)
         self.create_subscription(Int32MultiArray, '/lift_stat', self.lift_stat_callback, 10)
         self.create_subscription(Alive, '/alive', self.alive_callback, 10)
-        self.websocket_url = 'ws://localhost:6759'
-        asyncio.run(self.send_via_websocket())
+        self.websocket_url = 'ws://localhost:8765'
+
+        self.websocket_thread = threading.Thread(target=self.run_websocket_client)
+        self.websocket_thread.daemon = True
+        self.websocket_thread.start()
+
+    def listener_callback(self, msg):
+        self.get_logger().info('Receiving message: "%s"' % msg.data)
+        self.message_queue.append(msg.data)
+
+    async def websocket_client(self):
+        uri = "ws://localhost:8765"  # 웹소켓 서버 주소 및 포트
+        async with websockets.connect(uri) as websocket:
+            while True:
+                if self.message_queue:
+                    message = self.message_queue.popleft()
+                    await websocket.send(message)
+                    self.get_logger().info('Sending message: "%s"' % message)
+                else:
+                    await asyncio.sleep(0.1)
+
+    def run_websocket_client(self):
+        asyncio.new_event_loop().run_until_complete(self.websocket_client())
+        # threading.Thread(target=self.queue_feeder, daemon=True).start()
+        # asyncio.run(self.send_to_websocket())
+
+
+        # asyncio.run(self.send_to_websocket())
+
+    # def queue_feeder(self):
+    #     j = 1
+    #     while True:
+    #         self.deque.append(f"test - {j}")
+    #         asyncio.create_task(self.send_to_websocket(f"test - {j}"))
+            
+    #         time.sleep(0.1)
 
 
     def basic_stat_callback(self, msg):
@@ -84,32 +119,51 @@ class MyNode(Node):
     #     while True:
     #         
         
-    async def send_via_websocket(self):
-        async with websockets.connect(self.websocket_url) as websocket:
-            while True:
-                if self.deque:
-                    # data = self.deque.popleft()
-                    # buf = '{' + f'{data[0]}'+':'+f"{data[1]}"+ "}"
-                    # print(buf)
-                    await websocket.send('test')  # Emit data to the /stat route
-                else:
-                    print("nothing to publish")
-                    await asyncio.sleep(0.05)
-            # 서버 응답을 받고 싶다면 여기서 대기할 수 있습니다.
+    # async def send_via_websocket(self):
+    #     async with websockets.connect(self.websocket_url) as websocket:
+    #         i = 0
+    #         while True:
+    #             # await websocket.send(f'test {i}')  # Emit data to the /stat route
+    #             await asyncio.sleep(0.1)
+    #             i = i+1
+
+    #             if self.deque:
+    #                 data = self.deque.popleft()
+    #                 # buf = '{' + f'{data[0]}'+':'+f"{data[1]}"+ "}"
+    #                 # print(buf)
+    #                 await websocket.send(str(data))  # Emit data to the /stat route
+    #             else:
+    #                 print("nothing to publish")
+    #                 await asyncio.sleep(0.05)
+    #         # 서버 응답을 받고 싶다면 여기서 대기할 수 있습니다.
 
     # def send_msg(self, message):
-        
+    
+    # async def send_to_websocket(self, ):
+    #     async with websockets.connect(self.websocket_url) as websocket:
+    #         # self.get_logger().info('sending websocket: "%s"' % message)
+    #         if self.deque:
+    #             await websocket.send('test')
+    #         else:
+    #             await asyncio.sleep(0.1)
+
     
 
 def main(args=None):
     print("ws_started")
     rclpy.init(args=args)
     node = MyNode()
+
+    # loop = asyncio.get_event_loop()
+   
     try:
         rclpy.spin(node)
+        # loop.run_until_complete(rclpy.spin(node))
     except KeyboardInterrupt:
         pass
     node.destroy_node()
+    # loop.close()
+
     rclpy.shutdown()
 
 if __name__ == '__main__':
